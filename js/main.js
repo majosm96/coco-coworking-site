00e40a43635e172f67d3e6203a70ec69f90bd8e4
Fernanda Fajardo T <mfajardo@b7-09.local>
2017-07-12 13:48:51 -0600

    New Changes in Eventos

--- js/main.js
+++ js/main.js
@@ -28,3 +28,28 @@ btnleft.addEventListener('click', function(){
 btnright.addEventListener('click', function(){
 	plusDivs(1);
 });
+
+
+$(function() {
+	$.fn.scrollToTop=function(){
+		$(this).hide().removeAttr("href");
+		if($(window).scrollTop()!="0"){
+			$(this).fadeIn("slow")
+		}
+		var scrollDiv=$(this);
+		$(window).scroll(function(){
+			if($(window).scrollTop()=="0"){
+				$(scrollDiv).fadeOut("slow")
+			}else {
+				$(scrollDiv).fadeIn("slow")
+			} 
+		});
+		$(this).click(function(){
+			$("html, body").animate({scrollTop:0},"slow")
+		});
+	};
+});
+        
+$(function(){
+	$("#toTop").scrollToTop();
+});
